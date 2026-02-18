2e83e40221f7a1fc74f0c3e34845f1a903a1a162
Ravi-nn <ravichurnia23@gmail.com>
2026-02-18 22:02:51 +0530

    Full project sync

--- config/settings.py
+++ config/settings.py
@@ -61,7 +61,9 @@ DATABASES = {
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+if not DEBUG:
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
