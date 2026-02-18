d6132db70a6142575a5a008300fabfa0b6fe96ce
Ravi-nn <ravichurnia23@gmail.com>
2026-02-18 22:11:58 +0530

    Full project sync

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'your-secret-key'
+SECRET_KEY = 'c!)@8kzqb&8jfrqj6_dapb=w)$vra4d**n@&9(rr0#hp+f4!bf'
 
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
